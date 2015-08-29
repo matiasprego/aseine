b708de874bd0bd140e5354baad340f043436073b
Matías Prego <yo@matiasprego.com>
2015-08-29 17:53:30 -0300

    remove include footer

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -1,12 +1,3 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
-    require_once( get_stylesheet_directory() . '/footer.php' );
-}
 ?>
